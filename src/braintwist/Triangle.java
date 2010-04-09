8138ad70f30f72f9734349abf05e1ba3e5bd6d95
Victor <mrweektor@gmail.com>
2010-04-09 16:32:34 -0700

    restructuring

--- src/braintwist/Triangle.java
+++ src/braintwist/Triangle.java
@@ -4,26 +4,14 @@ import java.util.*;
 import java.io.*;
 
 public class Triangle {
-	private Point p0;
-	private Point p1;
-	private Point p2;
+	public int[] points;
 	
-	public Triangle(Point p0, Point p1, Point p2){
-		this.p0 = p0;
-		this.p1 = p1;
-		this.p2 = p2;
+	public Triangle(int p0, int p1, int p2){
+		points = new int[3];
+		points[0] = p0;
+		points[1] = p1;
+		points[2] = p2;
 	}
 	
-	public void rotateCW(){
-		Point t = new Point(p0.petal);
-		p0.petal = p1.petal;
-		p1.petal = p2.petal;
-		p2.petal = t.petal;
-	}
-	public void rotateCCW(){
-		Point t = new Point(p0.petal);
-		p0.petal = p2.petal;
-		p2.petal = p1.petal;
-		p1.petal = t.petal;
-	}
+	
 }
